e8849e39836ff37d6786d94c3f91c175435943d2
ryan-spearman <ryanspearman@gmail.com>
2022-03-10 19:27:19 -0600

    Achieved Functionality via JS wherein  Current Weather Data is being Displayed to Main Display Element

--- app.js
+++ app.js
@@ -3,37 +3,54 @@ const API_URL = `https://api.weather.gov/gridpoints/LSX/81,26/forecast`
 
 const previousWeatherToggle = document.querySelector('.show-previous-weather-label')
 const previousWeather = document.querySelector('.previous-weather')
+const currentTimeEle = document.querySelector('[data-current-time]')
+const currentDateEle = document.querySelector('[data-current-date]')
+const currentTempHighEle = document.querySelector('[data-current-temp-high]')
+const forecastSummaryEle = document.querySelector('[data-forecast-summary]')
+const windSpeedEle = document.querySelector('[data-wind-speed]')
+const windDirectionTextEle = document.querySelector('[data-wind-direction-text]')
 
 previousWeatherToggle.addEventListener('click', () => {
     previousWeather.classList.toggle('show-weather')
 })
 
-getWeather()
+let featuredWeatherIndex
+
+getWeather().then().then(weather => {
+    featuredWeatherIndex = weather[0]
+    console.log(weather)
+    displayFeaturedWeather(weather)
+})
+
+function displayFeaturedWeather(weather) {
+    const featuredWeather = featuredWeatherIndex
+    currentTimeEle.innerText = featuredWeatherIndex.timeWindow
+    currentDateEle.innerText = featuredWeatherIndex.date
+    currentTempHighEle.innerText = featuredWeatherIndex.tempString
+    forecastSummaryEle.innerText = featuredWeatherIndex.forecastSum
+    windSpeedEle.innerText = featuredWeatherIndex.windSpd
+    windDirectionTextEle.innerText = featuredWeatherIndex.windDir
+} 
+
 function getWeather() {
-    fetch(API_URL)
+    return fetch(API_URL)
     .then(res => res.json())
     .then(data => {
         const {
             ...weatherData
         } = data
-        const temp = Object.entries(weatherData.properties.periods).map(([weather, data]) => {
+        return Object.entries(weatherData.properties.periods).map(([weather, data]) => {
             return {
                 currentCond: data.shortForecast,
                 timeWindow: data.name,
                 tempNum: data.temperature,
-                tempstring: `${data.temperature}°${data.temperatureUnit}`,
+                tempString: `${data.temperature}°${data.temperatureUnit}`,
                 windDir: data.windDirection,
                 windSpd: data.windSpeed,
                 forecastSum: data.detailedForecast,
+                date: new Date(data.startTime),
             }
         })
-        console.log(weatherData.properties.periods)
-        console.log(weatherData.properties.periods[4].name)
-        console.log(`${weatherData.properties.periods[4].temperature}°${weatherData.properties.periods[4].temperatureUnit}`) 
-        console.log(weatherData.properties.periods[4].icon)
-        console.log(weatherData.properties.periods[4].windDirection)
-        console.log(weatherData.properties.periods[4].windSpeed)
-        console.log(weatherData.properties.periods[4].detailedForecast)
         console.log(temp)
     })
 }
